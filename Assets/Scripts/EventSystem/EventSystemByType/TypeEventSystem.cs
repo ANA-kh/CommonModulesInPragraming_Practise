49237e7a4f33fd2bbf14219b95e4fd15fe7a6502
ANA_H <1154073432@qq.com>
2022-05-08 11:57:46 +0800

    change eventSystem Interface

--- Assets/Scripts/EventSystem/EventSystemByType/TypeEventSystem.cs
+++ Assets/Scripts/EventSystem/EventSystemByType/TypeEventSystem.cs
@@ -5,15 +5,54 @@ namespace EventSystem.EventSystemByType
 {
     public interface ITypeEventSystem
     {
-        void Send<T>() where T : new();
-        void Send<T>(T e);
-        IUnRegister Register<T>(Action<T> onEvent);
-        void UnRegister<T>(Action<T> onEvent);
+        void Send(TypeEventArgs e);
+        IUnRegister Register(Action<TypeEventArgs> onEvent);
+        void UnRegister(Action<TypeEventArgs> onEvent);
+    }
+    
+    public abstract class TypeEventArgs
+    {
+            
     }
 
     public interface IUnRegister
     {
         void UnRegister();
+    }
+    
+    public struct TypeEventSystemUnRegister<T> : IUnRegister
+    {
+        public ITypeEventSystem TypeEventSystem;
+        public Action<TypeEventArgs> OnEvent;
+        public void UnRegister()
+        {
+            TypeEventSystem.UnRegister(OnEvent);
+            TypeEventSystem = null;
+            OnEvent = null;
+        }
+    }
+    
+    public class TypeEventSystem : ITypeEventSystem
+    {
+        
+
+        private Dictionary<Type, Action<TypeEventArgs>> _eventDic;
+
+
+        public void Send(TypeEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUnRegister Register(Action<TypeEventArgs> onEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnRegister(Action<TypeEventArgs> onEvent)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
