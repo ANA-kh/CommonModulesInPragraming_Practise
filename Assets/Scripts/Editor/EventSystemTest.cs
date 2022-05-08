3b69871e76667e2ccf5b7026f61fc0f3a0e94d2e
ANA_H <1154073432@qq.com>
2022-05-08 13:15:26 +0800

    change eventSystem Interface

--- Assets/Scripts/Editor/EventSystemTest.cs
+++ Assets/Scripts/Editor/EventSystemTest.cs
@@ -1,11 +1,35 @@
-﻿using NUnit.Framework;
+﻿using System;
+using EventSystem.EventSystemByType;
+using NUnit.Framework;
 using UnityEngine;
 
 namespace EventSystem
 {
     public class EventSystemTest
     {
-        
+        [Test]
+        public void TypeSystemTest()
+        {
+            TypeEventSystem typeEventSystem = new TypeEventSystem();
+            var unRegister = typeEventSystem.Register<TestEventArgs>(
+                e =>
+                {
+                    e.x = 10;
+                });
+            
+            var eventArgs = new TestEventArgs();
+            
+            typeEventSystem.Send(eventArgs);
+            Assert.Equals(10,eventArgs.x);
+            eventArgs.x = 2;
+            unRegister.UnRegister();
+            typeEventSystem.Send(eventArgs);
+            Assert.Equals(2,eventArgs.x);
+        }
+        public class TestEventArgs : TypeEventArgs
+        {
+            public int x = 0;
+        }
         
     }
 }
--- Assets/Scripts/EventSystem/EventSystemByType/TypeEventSystem.cs
+++ Assets/Scripts/EventSystem/EventSystemByType/TypeEventSystem.cs
@@ -5,14 +5,10 @@ namespace EventSystem.EventSystemByType
 {
     public interface ITypeEventSystem
     {
-        void Send(TypeEventArgs e);
-        IUnRegister Register(Action<TypeEventArgs> onEvent);
-        void UnRegister(Action<TypeEventArgs> onEvent);
-    }
-    
-    public abstract class TypeEventArgs
-    {
-            
+        void Send<T>() where T : new();
+        void Send<T>(T e);
+        IUnRegister Register<T>(Action<T> onEvent); //返回注销对象，避免忘记注销
+        void UnRegister<T>(Action<T> onEvent);
     }
 
     public interface IUnRegister
@@ -20,39 +16,4 @@ namespace EventSystem.EventSystemByType
         void UnRegister();
     }
     
-    public struct TypeEventSystemUnRegister<T> : IUnRegister
-    {
-        public ITypeEventSystem TypeEventSystem;
-        public Action<TypeEventArgs> OnEvent;
-        public void UnRegister()
-        {
-            TypeEventSystem.UnRegister(OnEvent);
-            TypeEventSystem = null;
-            OnEvent = null;
-        }
-    }
-    
-    public class TypeEventSystem : ITypeEventSystem
-    {
-        
-
-        private Dictionary<Type, Action<TypeEventArgs>> _eventDic;
-
-
-        public void Send(TypeEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IUnRegister Register(Action<TypeEventArgs> onEvent)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UnRegister(Action<TypeEventArgs> onEvent)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
-
