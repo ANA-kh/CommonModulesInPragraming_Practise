74eac2569176731a28f987e3c97b31e4b0b7b435
ANA_H <1154073432@qq.com>
2022-05-08 11:30:32 +0800

    add eventSystem Interface

---
+++ Assets/Scripts/Editor/EventSystemTest.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace EventSystem
+{
+    public class EventSystemTest
+    {
+        
+        
+    }
+}
---
+++ Assets/Scripts/EventSystem/EventSystemByType/TypeEventSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventSystem.EventSystemByType
+{
+    public interface ITypeEventSystem
+    {
+        void Send<T>() where T : new();
+        void Send<T>(T e);
+        void Register<T>(IEvent onEvent);
+        void UnRegister<T>(IEvent onEvent);
+    }
+
+    public interface IUnRegister
+    {
+        void UnRegister();
+    }
+    
+    public interface IEvent
+    {
+        void OnEvent();
+    }
+    
+    
+}
+
