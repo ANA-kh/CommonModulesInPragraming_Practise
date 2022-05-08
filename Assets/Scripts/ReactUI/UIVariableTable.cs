d45eb80abd216b3d1be60629e785c8649db558a4
ANA_H <1154073432@qq.com>
2022-05-08 20:09:11 +0800

    start ReactUI

---
+++ Assets/Scripts/ReactUI/UIVariableTable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace ReactUI
+{
+    [AddComponentMenu("ReactUI/UI/Bind/UI Variable Table")]
+    public sealed class UIVariableTable : MonoBehaviour
+    {
+        
+    }
+
+    public sealed class UIVariable
+    {
+        
+    }
+}
